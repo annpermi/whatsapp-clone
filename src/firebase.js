aceefefa625cfc2c58efbc427d7ed8f16be13826
annpermi <perminova.anna85@gmail.com>
2021-06-13 17:41:07 -0400

    Created app

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+//Connect database
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDyamkO-hFkMAi2ZWAcFReDZi-0DlhMc18",
+    authDomain: "whatsapp-clone-34161.firebaseapp.com",
+    projectId: "whatsapp-clone-34161",
+    storageBucket: "whatsapp-clone-34161.appspot.com",
+    messagingSenderId: "469958437826",
+    appId: "1:469958437826:web:8162472b7617f8abf5045b",
+    measurementId: "G-2Q9HZHDBT3"
+  };
